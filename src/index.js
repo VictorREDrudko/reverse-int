067e6d0c8a91e5b33955e491b000fc454c4541d9
Viktor Rudko <viktor_RED_rudko@mail.ru>
2023-08-04 14:12:32 +0300

    docs:result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+	if(n > 0) {
+		const stringN = String(n)
+		 if(stringN.length == '2') {return + (stringN[1] + stringN[0])}
+		 if(stringN.length == '3') {return + (stringN[2] + stringN[1] + stringN[0])}
+		 if(stringN.length == '4') {return + (stringN[3] + stringN[2] + stringN[1] + stringN[0])}
+}
+
+	if(n < 0) {
+		const stringN = String(-n)
+		 if(stringN.length == '2') {return + (stringN[1] + stringN[0])}
+		 if(stringN.length == '3') {return + (stringN[2] + stringN[1] + stringN[0])}
+		 if(stringN.length == '4') {return + (stringN[3] + stringN[2] + stringN[1] + stringN[0])}
+	}
 }
